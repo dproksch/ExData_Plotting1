cf62bb06ab39eeabd0594e0b7437135abac9c5aa
David Proksch - IBM <proksch@us.ibm.com>
2017-04-05 08:11:34 -0400

    committing plot1 files

--- IOLibs.R
+++ IOLibs.R
@@ -1,4 +1,5 @@
-library(dplyr)
+library(data.table)
+library(lubridate)
 #
 # This is the I/O library for Data Plotting instead of replicating code 
 # in four (4) source files, import this library and use the pre-defined 
@@ -6,10 +7,21 @@ library(dplyr)
 #
 
 readFile <- function() {
-  f <- read.csv(file = 'data/household_power_consumption.txt', header = TRUE, sep = ';')
-  df = data.frame(f) 
-  df$Date <- as.Date(df$Date,  format= "%d/%m/%Y")
-  subset(df, Date >= "2007/02/01" & Date <= "2007/02/02")
+  f <- fread("data/household_power_consumption.txt", sep=";", header=TRUE, na.strings="?", colClasses=rep("chr",9))
+  
+  #df = data.frame(f) 
+  #df<-df  %>% 
+  f <- f %>%
+    filter(Date == "1/2/2007" | Date == "2/2/2007") %>% 
+    mutate(Date=dmy(Date), 
+           Time=as.ITime(strptime(Time, "%H:%M:%S")), 
+           Global_active_power=as.numeric(Global_active_power),
+           Global_reactive_power=as.numeric(Global_reactive_power),
+           Voltage=as.numeric(Voltage),
+           Global_intensity=as.numeric(Global_intensity),
+           Sub_metering_1=as.numeric(Sub_metering_1),
+           Sub_metering_2=as.numeric(Sub_metering_2),
+           Sub_metering_3=as.numeric(Sub_metering_3))
 }
 
 # for testing purposes
--- plot1.R
+++ plot1.R
@@ -1,4 +1,8 @@
 source(file = "./IOLibs.R")
 
 df <- readFile()
-head(df)
+#head(df)
+#str(df)
+png("plot1.png", width = 480, height = 480, units="px")
+with(df, hist(Global_active_power, breaks=12, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red"))
+dev.off()
